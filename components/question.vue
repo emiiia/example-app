59bbe53bab2aec14c314681ad9129ac6b811fabc
Emilia Lukose <emilia24k@gmail.com>
2021-04-21 18:00:55 +0100

    shadow

--- components/question.vue
+++ components/question.vue
@@ -92,6 +92,7 @@ export default {
 .img-container {
   overflow: hidden;
   border: 5px solid white;
+  box-shadow:  0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
 
 .img-selected {
